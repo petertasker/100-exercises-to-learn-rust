64f60cd029cf9cd5583779e0764c5607f91517be
Peter Tasker <peter.tasker.2023@uni.strath.ac.uk>
2025-04-08 16:23:43 +0100

    complete ex04_01

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -2,11 +2,24 @@
 // even, otherwise `false`.
 //
 // Then implement the trait for `u32` and `i32`.
+trait IsEven {
+    fn is_even(&self) -> bool;
+}
 
+impl IsEven for i32 {
+    fn is_even(&self) -> bool {
+        self % 2 == 0
+    }
+}
+
+impl IsEven for u32 {
+    fn is_even(&self) -> bool {
+        self % 2 == 0
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_u32_is_even() {
         assert!(42u32.is_even());
